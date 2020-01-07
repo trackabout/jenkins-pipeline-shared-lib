de47de481a8ead14cb8603f81f338ea133d129ba
Larry Silverman <lsilverman@trackabout.com>
2020-01-07 13:22:40 -0600

    Trying a milestone approach.

--- vars/cancelPreviousBuilds.groovy
+++ vars/cancelPreviousBuilds.groovy
@@ -1,15 +1,6 @@
 #!/usr/bin/env groovy
-def call() {
-    // Check for other instances of this particular build, cancel any that are older than the current one
-    def jobName = env.JOB_NAME
-    def currentBuildNumber = env.BUILD_NUMBER.toInteger()
-    def currentJob = Jenkins.instance.getItemByFullName(jobName)
-
-    // Loop through all instances of this particular job/branch
-    for (def build : currentJob.builds) {
-        if (build.isBuilding() && (build.number.toInteger() < currentBuildNumber)) {
-            echo "Older build still queued. Sending kill signal to build number: ${build.number}"
-            build.doStop()
-        }
-    }
+def cancelPreviousBuilds() {
+    def buildNumber = env.BUILD_NUMBER as int
+    if (buildNumber > 1) milestone(buildNumber - 1)
+    milestone(buildNumber)
 }
