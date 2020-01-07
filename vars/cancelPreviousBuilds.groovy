ae70a628fc56d0235a110a7cf28a3efb59fc81b1
Larry Silverman <lsilverman@trackabout.com>
2020-01-07 15:25:53 -0600

    Commenting

--- vars/cancelPreviousBuilds.groovy
+++ vars/cancelPreviousBuilds.groovy
@@ -1,4 +1,8 @@
 #!/usr/bin/env groovy
+
+// This is untested. Been trying a number of ways of
+// aborting the AWS_Master_Tests job if another run kicks off.
+// see https://stackoverflow.com/questions/40760716/jenkins-abort-running-build-if-new-one-is-started
 def cancelPreviousBuilds() {
     def buildNumber = env.BUILD_NUMBER as int
     if (buildNumber > 1) milestone(buildNumber - 1)
